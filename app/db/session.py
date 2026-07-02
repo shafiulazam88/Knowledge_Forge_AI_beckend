051d271816c7f6d3fb32759335a68d1b95baee9d
shafiulazam88 <shafiulazam88001143@gmail.com>
2026-07-03 00:02:49 +0600

    feat(db): migrate to async SQLALchemy architecture

--- app/db/session.py
+++ app/db/session.py
@@ -1,41 +1,52 @@
 #database engine
 
+
 # from sqlalchemy import create_engine
 # from sqlalchemy.orm import sessionmaker
 # from app.core.config import get_settings
 
 # settings = get_settings()
-
 # engine = create_engine(
-#     settings.database_url,
-#     echo=settings.debug,
+#         settings.database_url,
+#         echo=settings.debug,
 # )
 
-# # no auto commit, no auto flush, bind to engine
 # SessionLocal = sessionmaker(
 #     autocommit=False,
 #     autoflush=False,
 #     bind=engine,
 # )
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+from collections.abc import AsyncGenerator
+from sqlalchemy.ext.asyncio import (
+      create_async_engine,
+      AsyncSession,
+      async_sessionmaker,
+)
+
 from app.core.config import get_settings
 
 settings = get_settings()
-engine = create_engine(
-        settings.database_url,
-        echo=settings.debug,
+
+engine = create_async_engine(
+    settings.database_url,
+    echo=settings.debug,
+    pool_pre_ping=True,
 )
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
+SessionLocal = async_sessionmaker(
     bind=engine,
+    autoflush=False,
+    expire_on_commit=False 
 )
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+async def get_db()-> AsyncGenerator[AsyncSession, None]:
+    async with SessionLocal() as session:
+        yield session
