e37a752ec671fc24e5359e2e7fe4a8dbe46f39e8
shafiulazam88 <shafiulazam88001143@gmail.com>
2026-07-04 15:23:59 +0600

    feat(users): implement service layer for registration

---
+++ app/modules/users/service.py
@@ -0,0 +1,29 @@
+
+from app.modules.users.schema import UserRegister, UserResponse
+from app.modules.users.repository import UserRepository
+from app.core.secuirity import hash_password
+from app.modules.users.model import User
+
+# the flow data will come from client then validate it using pydantic schema then pass it to service layer for business logic
+#  then pass it to repository layer for database operations 
+# then return the response to client
+class UserService:
+    def __init__(self, repository: UserRepository):
+        self.repository = repository
+    
+    async def register(
+            self,
+            data: UserRegister
+    )->User:
+        existing_user = await self.repository.get_bye_email(data.email)
+        if existing_user:
+            raise ValueError("User with this email already exists")
+        
+        user = User(
+            email=data.email,
+            password_hash=hash_password(data.password),
+            full_name=data.full_name
+        )
+
+        return await self.repository.create(user)
+        
