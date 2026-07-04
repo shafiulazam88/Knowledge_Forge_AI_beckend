850d69a18194f684dbfc0680df32472d06e958e3
shafiulazam88 <shafiulazam88001143@gmail.com>
2026-07-04 13:22:37 +0600

    feat(users): add request and response schema

---
+++ app/modules/users/schema.py
@@ -0,0 +1,14 @@
+# only validation pydantic schema
+from pydantic import BaseModel , ConfigDict , EmailStr , Field
+
+class UserRegister(BaseModel):
+     email : EmailStr
+     password : str = Field(min_length=8, max_length=128)
+     full_name: str = Field(min_length=2, max_length=100)
+
+class UserResponse(BaseModel):
+     id: str
+     email: EmailStr
+     full_name: str
+     role : str
+     model_config=ConfigDict(from_attributes=True)
